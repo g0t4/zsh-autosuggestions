f66789e4ca03a98d4ff6011970e7305928fb59ee
Wes Higbee <wes.mcclure@gmail.com>
2024-11-25 02:45:12 -0600

    read ask_service config from fish shell univ var

--- src/strategies/copilot.zsh
+++ src/strategies/copilot.zsh
@@ -19,10 +19,21 @@ _zsh_autosuggest_strategy_copilot() {
 		pattern="($pattern)~($ZSH_AUTOSUGGEST_HISTORY_IGNORE)"
 	fi
 
+    # *** CACHE ask_service params (which are changed in fish shell currently)
+    if [[ -z $ask_service ]]; then
+        # *** FYI this is cached per shell process, takes 40ms to query so yeah I don't wanna slow down my prototype, I need to see how it feels with minimal lag and in a real impl I could easily invalidate my zsh cache when I change it in fish and/or vice versa
+        # read value from fish shell universal variable named "ask_service"
+        ask_service=$(fish -c 'echo $ask_service')
+    fi
 
+    # ! TODO wire up to copilot codex completions
+    # TODO wire up to copilot chat completions
+    # TODO add context to prompt, i.e. recent command history
+    #    I suspect recent history will make it easier to build commands from a short prefix, dotnet build, do => dotnet run!
+    #    later, try PWD/list files in PWD or tree with limits
 	_python3="${WESCONFIG_DOTFILES}/.venv/bin/python3"
 	_single_py="${WESCONFIG_DOTFILES}/zsh/universals/3-last/ask-openai/single.py"
-	response=$( $_python3 $_single_py 2>&1 \
+	response=$( $_python3 $_single_py $ask_service 2>&1 \
             <<STDIN_CONTEXT
 env: zsh on $(uname)
 task: you are a strategy in zsh-autosuggetions, to complete the following prompt, your response must repeate the prompt verbatim and then the rest of the command that you are suggesting.
